63b478045f10b61196b37e57d05c39ffac7fd0e6
Firstname Lastname <your_email@youremail.com>
2014-06-26 12:29:57 +0300

    optimizer added

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -12,9 +12,9 @@ NEO4J_WRAPPER_PROP_FILE="${OPENSHIFT_NEO4J_DIR}/versions/2.1/neo4j-2.1/conf/neo4
  memory_total=`free -m | grep Mem | awk '{print $2}'`;
  [ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}";  }
 
-$SED -i "s/^#wrapper.java.initmemory/wrapper.java.initmemory/g";
+$SED -i "s/^#wrapper.java.initmemory/wrapper.java.initmemory/g" $NEO4J_WRAPPER_PROP_FILE;
 $SED -i "s/^wrapper.java.initmemory=.*/wrapper.java.initmemory=${XMS}/" $NEO4J_WRAPPER_PROP_FILE;
 
-$SED -i "s/^#wrapper.java.maxmemory/wrapper.java.maxmemory/g";
+$SED -i "s/^#wrapper.java.maxmemory/wrapper.java.maxmemory/g" $NEO4J_WRAPPER_PROP_FILE;
 $SED -i "s/^wrapper.java.maxmemory=.*/wrapper.java.maxmemory=${XMX}/" $NEO4J_WRAPPER_PROP_FILE;
 
